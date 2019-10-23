7cf6841e952c0a3c44acab97ab6bca55e96c52e2
Thomas Falcone <thomas.falcone06@gmail.com>
2019-10-23 19:22:12 +0200

    Add staging deploy if pushing on dev

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,15 @@ eval "$(ssh-agent -s)"
 chmod 600 ./deploy.key
 ssh-add ./deploy.key
 ssh-keyscan igpolytech.fr >> ~/.ssh/known_hosts
-git remote add deploy dokku@igpolytech.fr:admitech-back
-git config --global push.default simple
-git push deploy master
+if [ "$1" = "production" ]; then
+  git remote add deploy dokku@igpolytech.fr:admitech-back
+  git config --global push.default simple
+  git push deploy master
+elif [ "$1" = "staging" ]; then
+  git remote add deploy dokku@igpolytech.fr:admitech-back-staging
+  git config --global push.default simple
+  git push deploy dev
+else
+  echo "Incorrect environment, usage deploy.sh [production | staging]"
+	exit 1
+fi
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,6 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World in test mode!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
